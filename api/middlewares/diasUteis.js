b84b3205d23ed56e75691d352490cd474f6dbb29
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-05-12 16:09:09 -0300

    ✨ feat: Middleware do dias Uteis

---
+++ api/middlewares/diasUteis.js
@@ -0,0 +1,12 @@
+function permitirSomenteDiasUteis(req, res, next) {
+  const hoje = new Date();
+  const diaSemana = hoje.getDay(); // 0 = Domingo, 6 = Sábado
+
+  if (diaSemana === 0 || diaSemana === 6) {
+    return res.status(403).json({ mensagem: 'Acesso permitido apenas de segunda à sexta-feira.' });
+  }
+
+  next(); // continua para a próxima função/middleware/rota
+}
+
+module.exports = permitirSomenteDiasUteis;
