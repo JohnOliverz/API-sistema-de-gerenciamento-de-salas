64b1c25cd9acc99e129cfa9320abcd38745f77b2
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-05-12 16:05:35 -0300

    ✨ feat: Rotas Cadastrar Laboratórios e Gerar Relatórios

--- api/controller/laboratorio.controller.js
+++ api/controller/laboratorio.controller.js
@@ -1,5 +1,8 @@
 const Laboratorio = require('../models/Laboratorio');
+const gerarRelatorioLaboratorios = require('../utils/gerarRelatorioLaboratorios');
+const path = require('path');
 
+// Listar todos os laboratórios
 async function listarLaboratorios(req, res) {
   try {
     const laboratorios = await Laboratorio.find();
@@ -10,6 +13,49 @@ async function listarLaboratorios(req, res) {
   }
 }
 
+// Cadastrar um novo laboratório
+async function cadastrarLaboratorio(req, res) {
+  try {
+    const { nome, descricao, capacidade, foto } = req.body;
+
+    const novoLaboratorio = new Laboratorio({
+      nome,
+      descricao,
+      capacidade,
+      foto,
+    });
+
+    const laboratorioSalvo = await novoLaboratorio.save();
+    res.status(201).json(laboratorioSalvo);
+  } catch (erro) {
+    res.status(500).json({ erro: 'Erro ao cadastrar laboratório' });
+  }
+}
+
+// Gerar relatório em PDF
+async function gerarRelatorio(req, res) {
+  try {
+    const laboratorios = await Laboratorio.find();
+    const outputPath = path.join(__dirname, '..', 'relatorio_laboratorios.pdf');
+
+    await gerarRelatorioLaboratorios(laboratorios, outputPath);
+
+    res.download(outputPath, 'relatorio_laboratorios.pdf', (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Erro ao baixar o PDF.');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensagem: 'Erro ao gerar relatório' });
+  }
+}
+
+
+
 module.exports = {
-  listarLaboratorios
+    listarLaboratorios,
+    cadastrarLaboratorio,
+    gerarRelatorio
 };
--- api/routes/laboratorio.routes.js
+++ api/routes/laboratorio.routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { listarLaboratorios } = require('../controller/laboratorio.controller');
+const { cadastrarLaboratorio, listarLaboratorios, gerarRelatorio} = require('../controller/laboratorio.controller');
 
+// GET - listar todos
 router.get('/', listarLaboratorios);
+
+// POST - cadastrar novo laboratório
+router.post('/novo', cadastrarLaboratorio);
+
+// GET - gerar relatório em PDF
+router.get('/relatorio', gerarRelatorio);
 
 module.exports = router;
