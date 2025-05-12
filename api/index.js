9ae2fd4f59f9659a6236f823bfb4c1a7c790e78f
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-05-12 16:06:19 -0300

    ♻️ refactor: Mudanças no Index

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const conectarBanco = require('./config/bd');
+const authRoutes = require('./routes/auth.routes');
 const rotasLaboratorios = require('./routes/laboratorio.routes');
+const autenticarToken = require('./middlewares/autenticacao');
+const permitirSomenteDiasUteis = require('./middlewares/diasUteis');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware para permitir apenas dias úteis
+app.use(permitirSomenteDiasUteis);
 app.use(express.json());
 
 // Conectar ao banco
 conectarBanco();
 
 // Rotas
-app.use('/api/laboratorios', rotasLaboratorios);
+app.use('/api/auth', authRoutes);
+app.use('/api/laboratorios', autenticarToken, rotasLaboratorios);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
