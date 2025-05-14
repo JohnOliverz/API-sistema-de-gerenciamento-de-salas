1ca8c62736116c63c8c9246e6c98c2a8049a2cbd
Gabriel Oliveira <81201671+Gabriel-Oliveira1@users.noreply.github.com>
2025-05-14 19:24:08 -0300

    ♻️ refactor: Rotas de autorização

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,55 +1,12 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const Usuario = require('../models/Usuario');
+const { cadastrarUsuario, logarUsuario } = require('../controller/auth.controller');
 
 const router = express.Router();
-const JWT_SECRET = 'segredo_super_secreto'; // ideal usar dotenv em produção
 
-// Cadastro de novo usuário
-router.post('/cadastrar', async (req, res) => {
-  const { nome, email, senha } = req.body;
+// POST - cadastrar novo usuário
+router.post('/cadastrar', cadastrarUsuario);
 
-  try {
-    const existeUsuario = await Usuario.findOne({ email });
-    if (existeUsuario) {
-      return res.status(400).json({ mensagem: 'E-mail já cadastrado' });
-    }
-
-    const senhaHash = await bcrypt.hash(senha, 10);
-
-    const novoUsuario = new Usuario({ nome, email, senha: senhaHash });
-    await novoUsuario.save();
-
-    res.status(201).json({ mensagem: 'Usuário cadastrado com sucesso' });
-  } catch (erro) {
-    res.status(500).json({ erro: 'Erro ao cadastrar usuário' });
-  }
-});
-
-// Login de usuário
-router.post('/logar', async (req, res) => {
-  const { email, senha } = req.body;
-
-  try {
-    const usuario = await Usuario.findOne({ email });
-    if (!usuario) {
-      return res.status(401).json({ mensagem: 'Usuário não encontrado' });
-    }
-
-    const senhaValida = await bcrypt.compare(senha, usuario.senha);
-    if (!senhaValida) {
-      return res.status(401).json({ mensagem: 'Senha inválida' });
-    }
-
-    const token = jwt.sign({ id: usuario._id, email: usuario.email }, JWT_SECRET, {
-      expiresIn: '1h',
-    });
-
-    res.json({ token });
-  } catch (err) {
-    res.status(500).json({ erro: 'Erro no servidor' });
-  }
-});
+// POST - logar usuário
+router.post('/logar', logarUsuario);
 
 module.exports = router;
