b99954978077f7a5fd28b2ca28f8802a2a92c05a
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-06-22 21:11:37 -0300

    ♻️ refactor: Atualização do Index para as novas Funções

--- api/index.js
+++ api/index.js
@@ -1,15 +1,25 @@
+const http = require('http');
+const path = require('path');
 const express = require('express');
 const conectarBanco = require('./config/bd');
 const authRoutes = require('./routes/auth.routes');
 const rotasLaboratorios = require('./routes/laboratorio.routes');
+const mensagemHome = require('./utils/mensagemHome'); 
 const autenticarToken = require('./middlewares/autenticacao');
 const permitirSomenteDiasUteis = require('./middlewares/diasUteis');
-const mensagemHome = require('./utils/mensagemHome'); 
+const socketIO = require('./socket');
+
 require('dotenv').config();
 
 const app = express();
+const server = http.createServer(app); // necessário para o socket
 
-// Pagina inicial
+socketIO.init(server); // inicia socket.io com esse server
+
+// Servir arquivos HTML/CSS/JS estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Página inicial
 app.get('/', (req, res) => {
   res.send(mensagemHome);
 });
@@ -18,12 +28,18 @@ app.get('/', (req, res) => {
 app.use(permitirSomenteDiasUteis);
 app.use(express.json());
 
-// Conectar ao banco
 conectarBanco();
 
-// Rotas
 app.use('/api/auth', authRoutes);
 app.use('/api/laboratorios', autenticarToken, rotasLaboratorios);
 
-
-module.exports = app;
+// Verifica se está rodando localmente ou em ambiente de produção
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  });
+} else {
+  // Exporta o app para o Vercel
+  module.exports = app;
+}
