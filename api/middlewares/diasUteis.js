5a5bd43ccda1936f00fff60699e3075f27c06345
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-06-22 21:12:12 -0300

    🧪 test: Alterando Middleware para teste em Produção

--- api/middlewares/diasUteis.js
+++ api/middlewares/diasUteis.js
@@ -2,7 +2,7 @@ function permitirSomenteDiasUteis(req, res, next) {
   const hoje = new Date();
   const diaSemana = hoje.getDay(); // 0 = Domingo, 6 = Sábado
 
-  if (diaSemana === 0 || diaSemana === 6) {
+  if (diaSemana === 2 || diaSemana === 6) {
     return res.status(403).json({ mensagem: 'Acesso permitido apenas de segunda à sexta-feira.' });
   }
 
